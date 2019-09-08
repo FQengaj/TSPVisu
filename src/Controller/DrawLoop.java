5a214dfd3e8adbbbdcf6c456a6dd88bb20a545c4
Florian Qengaj <florian.qengaj@gmail.com>
2019-09-08 14:05:47 +0200

    Start Animation Timer, Fork it to play around.

---
+++ src/Controller/DrawLoop.java
@@ -0,0 +1,22 @@
+package Controller;
+
+import Model.Graph;
+import javafx.animation.AnimationTimer;
+
+import java.util.LinkedList;
+
+public class DrawLoop extends AnimationTimer {
+
+    LinkedList<Graph> AnimatedGraph = new LinkedList<>();
+    public DrawLoop(Graph g) {
+        AnimatedGraph.add(g);
+        this.start();
+    }
+
+    @Override
+    public void handle(long now) {
+        if (AnimatedGraph.size()>1){
+
+        }
+    }
+}
